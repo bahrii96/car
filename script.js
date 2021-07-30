ebd78079839de3911a11ede0beb11a000b55e676
bahrii96 <81860681+bahrii96@users.noreply.github.com>
2021-07-30 15:06:07 +0300

    зробив анымацыю при скролы

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMobileMenu() {
+    document.querySelector('#menu').classList.toggle('active')
+    document.querySelector('.mobile__bar').classList.toggle('active')
+}
+function toggleRegistration() {
+    document.querySelector('#form').classList.toggle('active')
+    document.querySelector('.registration').classList.toggle('active')
+}
