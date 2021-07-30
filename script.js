992dc25c83bca0d758fe45f132ff5cb702c2137f
bahrii96 <81860681+bahrii96@users.noreply.github.com>
2021-07-30 17:09:01 +0300

    ДЖАВА СКРІПТ НА КНОПКУ БІЛЬШЕ

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function toggleRegistration() {
     document.querySelector('#form').classList.toggle('active')
     document.querySelector('.registration').classList.toggle('active')
 }
+
+// показати быльше
+
+function toggleMore() {
+    document.querySelector('#more').classList.toggle('active')
+    document.querySelector('.marka__more').classList.toggle('active')
+}
+// показати більше відгуки
+function toggleReviews() {
+    document.querySelector('#reviews').classList.toggle('active')
+    document.querySelector('.reviews__more').classList.toggle('active')
+}
