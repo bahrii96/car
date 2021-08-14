b1bb757b6332ae475239dd652f21e52af9e347ed
bahrii96 <81860681+bahrii96@users.noreply.github.com>
2021-08-15 01:23:27 +0300

    адаптив550

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function toggleMobileMenu() {
 function toggleRegistration() {
     document.querySelector('#form').classList.toggle('active')
     document.querySelector('.registration').classList.toggle('active')
+  button.innerText = "Сохранить";
 }
+ 
 
 // показати быльше
 
@@ -18,3 +20,35 @@ function toggleReviews() {
     document.querySelector('#reviews').classList.toggle('active')
     document.querySelector('.reviews__more').classList.toggle('active')
 }
+
+// кнопка показати більше
+document.querySelectorAll("button").forEach(function(el){
+  	el.addEventListener("click",function(){
+      if(!this.dataset.secondname)
+        return;
+       var tmp = this.innerHTML;
+       this.innerHTML = this.dataset.secondname;
+       this.dataset.secondname = tmp;
+    },false)
+})
+
+// Авто на складі
+function toggleAvailability() {
+    document.querySelector('#form_availability').classList.toggle('active')
+    document.querySelector('.availability').classList.toggle('active')
+}
+
+// авто в європі
+function toggleEurope() {
+    document.querySelector('#form_europe').classList.toggle('active')
+    document.querySelector('.europe').classList.toggle('active')
+}
+
+
+// кнопка закрити 
+ 
+//  document.getElementById('f1').onclick = function () {
+//     func(1);
+// };
+        //Update Content-Container Div 
+ 
